80e71c7f5ce5bb40e41b963fe2cdc94c2b71a4bd
sebek78 <sebasporek@gmail.com>
2022-06-20 21:25:52 +0200

    Check terminal events

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+use crossterm::{
+    event::{
+        read, DisableMouseCapture, EnableMouseCapture, Event, KeyCode, KeyEvent, KeyModifiers,
+    },
+    execute,
+};
+
+fn print_events() -> crossterm::Result<()> {
+    loop {
+        match read()? {
+            Event::Key(KeyEvent {
+                code: KeyCode::Char('c'),
+                modifiers: KeyModifiers::CONTROL,
+            }) => break,
+            Event::Key(event) => println!("{:?}", event),
+            Event::Mouse(event) => println!("{:?}", event),
+            Event::Resize(width, height) => println!("New size {}x{}", width, height),
+        }
+    }
+    Ok(())
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut stdout = std::io::stdout();
+    execute!(stdout, EnableMouseCapture);
+
+    print_events().unwrap();
+
+    execute!(stdout, DisableMouseCapture);
 }
